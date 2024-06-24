f2b8dc27d1d06ff3b6441b43852a33507162e568
OriDaer <d47709740@alumnos.itr.edu.ar>
2024-06-24 16:21:30 -0300

    ej1 y ej2 completos

--- src/Ejercicio1.jsx
+++ src/Ejercicio1.jsx
@@ -1,12 +1,24 @@
+import { useState } from "react"
+import React from 'react';
 export default function Ejercicio1() {
-
+  const [fromData,setFormData]=useState('');
+  
+  const handleChange=(event)=>{
+    setFormData(event.target.value);
+  };
   return (
     <div>
       <h2>Ejercicio 1: Lista desplegable</h2>
 
       <label>Seleccione una fruta: </label>
       {/* Crear lista de selección */}
-
+      <select onChange={handleChange}>
+        <option value="manzana">Manzana</option>
+        <option value="pera">Pera</option>
+        <option value="naranja">Naranja</option>
+        <option value="mandarina">Mandarina</option>
+      </select>
+      <p>Seleccionaste:{fromData}</p>
     </div>
   )
 }
--- src/Ejercicio2.jsx
+++ src/Ejercicio2.jsx
@@ -1,9 +1,52 @@
+import { useState } from "react"
+
 export default function Ejercicio2() {
+  const [fromData,setFormData]=useState({
+    nombre:'',
+    edad:''
+  });
   
+  const handleChange=(event)=>{
+    const{name, value}= event.target;
+    setFormData({
+      ...fromData,
+      [name]:value
+    });
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if(fromData.edad < 18){
+      window.alert('Eres menor de edad!');
+    }
+    if(fromData.edad >=18){
+      window.alert('Eres mayor de edad!');
+    }
+
+    };
+
   return (
     <div>
       <h2>Ejercicio 2: Formulario</h2>
       {/* Crear formulario */}
+      <form onSubmit={handleSubmit}>
+      <label>Nombre:</label>
+      <input
+      type="text"
+      id="nombre"
+      name="nombre"
+      value={fromData.nombre}
+      onChange={handleChange}
+      />
+      <input
+      type="number"
+      id="edad"
+      name="edad"
+      value={fromData.edad}
+      onChange={handleChange}
+      />
+      <p>{fromData.nombre} tiene {fromData.edad} años</p>
+      <button type="submit">Enviar</button>
+      </form>
     </div>
   )
 }
